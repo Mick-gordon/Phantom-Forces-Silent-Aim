20c6b4609a387fdf0edaba3ad8e0bf5bb67622fe
Mick Gordon <78802703+Mick-gordon@users.noreply.github.com>
2025-01-15 12:42:53 +0000

    Update ESPloader.lua

--- ESPloader.lua
+++ ESPloader.lua
@@ -1,6 +1,8 @@
 -- I Made This Open Source For People To Learn Off.
 repeat task.wait() until game:IsLoaded()
-getgenv().DELETEMOB = {RunningESP = false};
+if not DELETEMOBESP then
+    getgenv().DELETEMOBESP = false;
+end;
 
 local ExecutorName = identifyexecutor and string.lower(identifyexecutor()) or "unkown";
 local Script = [[repeat task.wait() until game:IsLoaded(); task.wait(5);]] .. game:HttpGet("https://raw.githubusercontent.com/Mick-gordon/Phantom-Forces-Silent-Aim/refs/heads/main/ESP.lua");
@@ -11,6 +13,8 @@ local DetectionBypass = [[
 
 ]];
 local ActorCapture = [[
+    getgenv().CapturedActors = {};
+
     local Connection;
     Connection = game.DescendantAdded:Connect(function(insance)
         if insance:IsA("Actor") then
@@ -24,11 +28,12 @@ local ActorCapture = [[
 ]];
 
 -- // Actor Bypass
-if not DELETEMOB.RunningESP then
+if not DELETEMOBESP then
+    
     if Success and string.lower(tostring(Output)) == "true" then
         loadstring(Script)();
+        DELETEMOBESP = true;
 
-        DELETEMOB.RunningESP = true;
     elseif string.match(ExecutorName, "awp1") then
         for _,Actor in getactors() do
             run_on_actor(Actor, [[
@@ -40,12 +45,12 @@ if not DELETEMOB.RunningESP then
                 end;
             ]]);
         end;
-
-        DELETEMOB.RunningESP = true;
+        DELETEMOBESP = true;
+        
     elseif string.match(ExecutorName, "wave") then
         run_on_actor(getdeletedactors()[1], Script)
+        DELETEMOBESP = true;
 
-        DELETEMOB.RunningESP = true;
     elseif string.match(ExecutorName, "nihon") then
         for _, Actor in getactorthreads() do
             run_on_thread(Actor, [[ 
@@ -56,10 +61,10 @@ if not DELETEMOB.RunningESP then
                     end;
                 end;
             ]]);
-        end;
+        end;  
+        DELETEMOBESP = true;
 
-        DELETEMOB.RunningESP = true;
-    elseif run_on_actor1 and queue_on_teleport then
+    elseif run_on_actor and queue_on_teleport then
         if CapturedActors then -- I Don't Trust Peoples getactos.
             repeat task.wait() until #CapturedActors >= 5;
             
@@ -73,43 +78,40 @@ if not DELETEMOB.RunningESP then
                     end;
                 ]]);
             end;
-            
-            DELETEMOB.RunningESP = true;
+            DELETEMOBESP = true;
+
         else
-            queue_on_teleport([[getgenv().CapturedActors = {};]] .. ActorCapture .. game:HttpGet("https://raw.githubusercontent.com/Mick-gordon/Phantom-Forces-Silent-Aim/refs/heads/main/ESPloader.lua"));
+            queue_on_teleport(ActorCapture .. game:HttpGet("https://raw.githubusercontent.com/Mick-gordon/Phantom-Forces-Silent-Aim/refs/heads/main/ESPloader.lua"));
             game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId);
 
         end;
 
-        DELETEMOB.RunningESP = true;
     elseif getfflag then
         local Success, Output = pcall(getfflag, "RunParallelLuaOnMainThread");
-        if (Success and string.lower(tostring(Output)) == "true") then
+        if Success and string.lower(tostring(Output)) == "true" then
             loadstring(Script)();
+            DELETEMOBESP = true;
 
         elseif queue_on_teleport and setfflag then
             if pcall(setfflag, "FFlagDebugRunParallelLuaOnMainThread", "True") then
                 queue_on_teleport(DetectionBypass .. "task.wait(5)" .. Script);
-                DELETEMOB.RunningESP = true;
+                DELETEMOBESP = true;
                 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId);
 
             elseif pcall(setfflag, "RunParallelLuaOnMainThread", "True") then -- Some Executors Just Fake It Or Something.
                 queue_on_teleport(DetectionBypass .. "task.wait(5)" .. Script);
-                DELETEMOB.RunningESP = true;
+                DELETEMOBESP = true;
                 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId);
 
             elseif pcall(setfflag, "RunParallelLuaOnMainThread", "true") then -- Got Me Tweaking.
                 queue_on_teleport(DetectionBypass .. "task.wait(5)" .. Script);
-                DELETEMOB.RunningESP = true;
+                DELETEMOBESP = true;
                 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId);
-
+                
             end;
         end;
 
     end;
 
-    if not DELETEMOB.RunningESP then -- If This Shit Dosent Then U Got A Nigga Executor. 
-        game:GetService("Players").LocalPlayer:Kick("Executor Is Not Supported End.");
-    end;
 end;
 
